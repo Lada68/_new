b93b138cb158fb0ff50ac6f7aafcf9bb1b8ba81b
dkhozyainov <dmitry.khozyainov@gmail.com>
2021-10-15 19:05:03 +0300

    products of category

--- src/main/java/com/amr/project/webapp/controller/MainPageController.java
+++ src/main/java/com/amr/project/webapp/controller/MainPageController.java
@@ -1,11 +1,12 @@
 package com.amr.project.webapp.controller;
 
-
 import com.amr.project.service.abstracts.ShowMainPageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @Controller
 public class MainPageController {
@@ -17,9 +18,15 @@ public class MainPageController {
         this.showMainPageService = showMainPageService;
     }
 
-    @GetMapping
+    @GetMapping("/")
     public String showMainPage(Model model) {
         model.addAttribute("mainPageDto", showMainPageService.show());
+        return "index";
+    }
+
+    @GetMapping("/category/{id}")
+    public String showMainCategory(Model model, @PathVariable Long id) {
+        model.addAttribute("mainPageDto", showMainPageService.findItemsByCategory(id));
         return "index";
     }
 }
