8d88be4313541e1a244d06613e844ec9175b6270
babai000888 <47792371+babai000888@users.noreply.github.com>
2021-11-02 20:50:59 +0300

    Пытаемся решить конфликт

--- src/main/resources/static/js/user_page/user_page_shops.js
+++ src/main/resources/static/js/user_page/user_page_shops.js
@@ -1,3 +1,19 @@
 function showUserShops() {
 
+
+    for(let shop of user.shops) {
+        console.log(shop)
+
+        let Shop = document.createElement('iframe')
+        Shop.className = "DELSHOP"
+        Shop.src = "/market/" + shop.id
+        Shop.height = "500px"
+        Shop.width = "500px"
+        // let newLi = document.createElement('li')
+        // newLi.className = "DELSHOP"
+        // newLi.href = "/market/" +
+
+        document.querySelector('.usershops').appendChild(Shop)
+    }
+
 }
